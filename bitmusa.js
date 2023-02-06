cbe8237c6d59873d39425ed6d3b416e2b6fdce35
satoshi-bitninja <satoshi@bitninja.dev>
2023-02-07 01:39:59 +0900

    Update bitmusa.js
    add balance function

--- bitmusa.js
+++ bitmusa.js
@@ -1,12 +1,90 @@
+const request = require('request');
+
 class Bitmusa {
     constructor(options = {}) {
-        this.options = options;
-        console.log(this.options);
+        if (typeof options === 'string') {
+            this.options = Object.assign(this.getDefaultOptions(), { authKey: options });
+        } else {
+            if (typeof options === 'object') {
+                if (Object.keys(options).length === 0) {
+                    this.options = this.getDefaultOptions();
+                } else {
+                    this.options = Object.assign(this.getDefaultOptions(), options);
+                }
+            } else {
+                throw new Error('[constructor] Invalid options');
+            }
+        }
+
     }
 
-    createWithKey(authKey ) {
+    getDefaultOptions() {
+        return {
+            baseURL: 'https://www.bitmusa.com/api',
+            timeOut: 1000
+        }
+    }
+
+    createWithKey(authKey) {
         return new Bitmusa({ authKey : authKey });
     }
+
+    setBaseURL(baseURL) {
+        this.options.baseURL = baseURL;
+    }
+
+    setAuthKey(authKey) {
+        this.options.authKey = authKey;
+    }
+
+    getBaseURL() {
+        return this.options.baseURL;
+    }
+
+    getAuthKey() {
+        return this.options.authKey;
+    }
+
+    buildRequestOptions(path, method, parameter = null) {
+
+        let options = {
+            url: this.options.baseURL + path,
+            method: method,
+            timeout: this.options.timeOut,
+            headers: {
+                'x-auth-token': this.options.authKey
+            }
+        };
+
+        if (parameter) {
+            options = Object.assign(options, { data: parameter });
+        }
+
+        return options;
+    }
+
+    balance() {
+        return new Promise((resolve, reject) => {
+            request(this.buildRequestOptions("/users/asset/wallet", 'GET'), (error, response, body) => {
+                if (error)
+
+                    reject(error);
+                else {
+                    if (response.statusCode !== 200) {
+                        reject("statusCode : " + response.statusCode);
+                    }
+
+                    let json = JSON.parse(body);
+                    if (json.code === 4000) {
+                        reject(json);
+                    } else {
+                        resolve(json);
+                    }
+                }
+            });
+        });
+    }
+
 }
 
 // export the class
