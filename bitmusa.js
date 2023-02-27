c34dd516ffa39895b75d2c44a589e382c80ca064
satoshi-bitninja <satoshi@bitninja.dev>
2023-02-28 04:06:38 +0900

    use axios instead of request

--- bitmusa.js
+++ bitmusa.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const axios = require('axios');
 const querystring = require('querystring');
 
 class Bitmusa {
@@ -12,13 +12,13 @@ class Bitmusa {
             };
         } else {
             this.options = this.getDefaultOptions();;
-        }            
+        }
     }
 
     getDefaultOptions() {
         return {
             baseURL: 'https://www.bitmusa.com/api',
-            timeout: 1000
+            timeout: 3000
         }
     }
 
@@ -47,79 +47,74 @@ class Bitmusa {
         return this.options.timeout;
     }
 
-    buildRequestOptions(path, method, parameter = null) {
-        const requestOptions = {
+
+
+    async requestAPI(path, method, data = null) {
+        method = method.toUpperCase();
+        const options = {
+            method: method,
             url: `${this.options.baseURL}${path}`,
-            json: true,
-            method: method.toUpperCase(),
-            timeout: this.options.timeout,
-            cache: false,
             headers: {
-              'x-auth-token': this.options.authKey,
-              'Content-Type': 'application/json'
-            }
+                'x-auth-token': this.options.authKey,
+                'Content-Type': 'application/json'
+            },
+            data: data,
+            responseType: 'json',
+            timeout: this.options.timeout
         };
-        
-        if (method.toUpperCase() === 'GET' && parameter) {
-            requestOptions.url += `?${querystring.stringify(parameter)}`;
-        } else if (parameter) { // when method is POST, PUT, DELETE, etc.
-            requestOptions.body = parameter;
+
+        try {
+            const response = await axios.request(options);
+            return response;
+        } catch (error) {
+            throw new Error(`Failed to requestAPI(${path}): ${error.message}`);
         }
-    
-        //console.log(requestOptions);
-    
-        return requestOptions;
+
     }
 
-    signIn(email, password) {
-        return new Promise((resolve, reject) => {
-            request(this.buildRequestOptions("/users/v1/signin", 'POST', { email: email, password: password }), (error, response, body) => {
-                
-                if (error)
-                    reject(error);
-                else {
-                    if (response.statusCode !== 200) {
-                        reject("statusCode : " + response.statusCode);
-                    }
 
-                    let json = typeof body === 'object' ? body : JSON.parse(body);
-                    if (json.code !== 0) {
-                        reject(json);
-                    } else {
-                        resolve(json);
-                    }
-                }
-            });
-        });
-    } 
+    async signIn(email, password) {
+        const funcName = '[signIn]:';
 
-    balance() {
-        return new Promise((resolve, reject) => {
-            request(this.buildRequestOptions("/users/asset/wallet", 'GET'), (error, response, body) => {
-                if (error)
+        if (!email) throw new Error(`${funcName} email is blank`);
+        if (!password) throw new Error(`${funcName} password is blank`);
 
-                    reject(error);
-                else {
-                    if (response.statusCode !== 200) {
-                        reject("statusCode : " + response.statusCode);
-                    }
+        try {
+            const response = await this.requestAPI('/users/v1/signin', 'post', { email: email, password: password });
+            if (response.status !== 200) throw new Error(`${funcName} ${response.status}`);
+            const json = response.data;
+            console.log(json);
+            if (json.code !== 0) {
+                throw new Error(`${funcName} ${response.data.message}[code:${json.code}]`);
+            }
 
-                    let json = typeof body === 'object' ? body : JSON.parse(body);
-                    if (json.code !== 0) {
-                        reject(json);
-                    } else {
-                        resolve(json);
-                    }
-                }
-            });
-        });
-    } 
+            return json;
+        } catch (error) {            
+            throw new Error(`${error.message}`);
+        }
+    }
+
+
+    async balance() {
+        try {
+            const options = this.buildRequestOptions('/users/asset/wallet', 'GET');
+            const response = await axios(options);
+            const json = response.data;
+            if (typeof json.code === 'number' && json.code !== 0) {
+                throw new Error(json.message);
+            }
+            return json;
+        } catch (error) {
+            throw new Error(`Failed to balance: ${error.message}`);
+        }
+    }
+
 
     buy(pair, amount, type = null, price = null) {
-        var options = { 
-            symbol : pair, 
-            amount : amount+"",
-            direction : 'BUY'
+        var options = {
+            symbol: pair,
+            amount: amount + "",
+            direction: 'BUY'
         };
 
         if ((type === null) || (type === 'MARKET_PRICE')) {
@@ -132,7 +127,7 @@ class Bitmusa {
                 throw new Error('[buy] price is null');
             }
 
-            options = Object.assign(options, { type: 'LIMIT_PRICE', price: price+"" });
+            options = Object.assign(options, { type: 'LIMIT_PRICE', price: price + "" });
         }
 
         return new Promise((resolve, reject) => {
@@ -146,20 +141,20 @@ class Bitmusa {
 
                     let json = typeof body === 'object' ? body : JSON.parse(body);
                     if (json.code !== 0) {
-                         reject(json);
+                        reject(json);
                     } else {
-                         resolve(json);
+                        resolve(json);
                     }
                 }
             });
         });
-    } 
+    }
 
     sell(pair, amount, type = null, price = null) {
-        var options = { 
-            symbol : pair, 
-            amount : amount+"",
-            direction : 'SELL'
+        var options = {
+            symbol: pair,
+            amount: amount + "",
+            direction: 'SELL'
         };
 
         if ((type === null) || (type === 'MARKET_PRICE')) {
@@ -172,7 +167,7 @@ class Bitmusa {
                 throw new Error('[sell] price is null');
             }
 
-            options = Object.assign(options, { type: 'LIMIT_PRICE', price: price+"" });
+            options = Object.assign(options, { type: 'LIMIT_PRICE', price: price + "" });
         }
 
         return new Promise((resolve, reject) => {
@@ -186,24 +181,24 @@ class Bitmusa {
 
                     let json = typeof body === 'object' ? body : JSON.parse(body);
                     if (json.code !== 0) {
-                         reject(json);
+                        reject(json);
                     } else {
-                         resolve(json);
+                        resolve(json);
                     }
                 }
             });
         });
     } // end of sell
 
-    order(direction = "buy", pair, amount, type = null, price = null) {        
+    order(direction = "buy", pair, amount, type = null, price = null) {
         direction = direction.toUpperCase();
         if (direction !== 'BUY' && direction !== 'SELL') {
             throw new Error('[order] direction is not BUY or SELL');
         }
-        var options = { 
-            symbol : pair, 
-            amount : amount+"",
-            direction : direction
+        var options = {
+            symbol: pair,
+            amount: amount + "",
+            direction: direction
         };
 
         if ((type === null) || (type === 'MARKET_PRICE')) {
@@ -216,7 +211,7 @@ class Bitmusa {
                 throw new Error('[sell] price is null');
             }
 
-            options = Object.assign(options, { type: 'LIMIT_PRICE', price: price+"" });
+            options = Object.assign(options, { type: 'LIMIT_PRICE', price: price + "" });
         }
 
         return new Promise((resolve, reject) => {
@@ -230,18 +225,18 @@ class Bitmusa {
 
                     let json = typeof body === 'object' ? body : JSON.parse(body);
                     if (json.code !== 0) {
-                         reject(json);
+                        reject(json);
                     } else {
-                         resolve(json);
+                        resolve(json);
                     }
                 }
             });
         });
-    } 
+    }
 
     cancel(orderId) {
         return new Promise((resolve, reject) => {
-            request(this.buildRequestOptions("/exchange/order/cancel/"+orderId, 'GET'), (error, response, body) => {
+            request(this.buildRequestOptions("/exchange/order/cancel/" + orderId, 'GET'), (error, response, body) => {
                 if (error)
                     reject(error);
                 else {
@@ -251,31 +246,31 @@ class Bitmusa {
 
                     let json = typeof body === 'object' ? body : JSON.parse(body);
                     if (json.code !== 0) {
-                         reject(json);
+                        reject(json);
                     } else {
-                         resolve(json);
+                        resolve(json);
                     }
                 }
             });
         });
-    } 
+    }
 
     cancelAll(targetSymbol = "", baseSymbol = "USDT", direction = "buy") {
         targetSymbol = targetSymbol.toUpperCase();
         baseSymbol = baseSymbol.toUpperCase();
         direction = direction.toUpperCase();
         const pair = `${targetSymbol}/${baseSymbol}`;
-        if (direction=="BUY") direction = 0;
-        else if (direction=="SELL") direction = 1;
+        if (direction == "BUY") direction = 0;
+        else if (direction == "SELL") direction = 1;
 
         if (direction !== 'BUY' && direction !== 'SELL') {
             throw new Error('[cancelAll] direction is not BUY or SELL');
         }
-        
+
         var options = {
-            symbol : pair,
-            direction : direction,
-            type : 1 // 0: Market Price, 1: Limit Price ??
+            symbol: pair,
+            direction: direction,
+            type: 1 // 0: Market Price, 1: Limit Price ??
         };
 
         return new Promise((resolve, reject) => {
@@ -296,7 +291,7 @@ class Bitmusa {
                 }
             });
         });
-    } 
+    }
 
 
     openOrders(page = 1, size = 10) {
@@ -311,16 +306,16 @@ class Bitmusa {
 
                     let json = typeof body === 'object' ? body : JSON.parse(body);
                     if (json.code !== 0) {
-                         reject(json);
+                        reject(json);
                     } else {
-                         resolve(json);
+                        resolve(json);
                     }
                 }
             });
         });
     }
 
-    ticker(targetSymbol="", baseSymbol="USDT") {
+    ticker(targetSymbol = "", baseSymbol = "USDT") {
         targetSymbol = targetSymbol.toUpperCase();
         baseSymbol = baseSymbol.toUpperCase();
         const pair = `${targetSymbol}/${baseSymbol}`;
@@ -336,11 +331,10 @@ class Bitmusa {
 
                     let json = typeof body === 'object' ? body : JSON.parse(body);
                     var ticker = json;
-                    if (targetSymbol!="")
-                    {
+                    if (targetSymbol != "") {
                         ticker = json.find((item) => item.symbol == pair);
                     }
-                    
+
                     if (ticker === undefined || ticker === null) {
                         reject("ticker not found");
                     } else {
@@ -352,7 +346,7 @@ class Bitmusa {
 
     }
 
-    price(targetSymbol="", baseSymbol="USDT") {
+    price(targetSymbol = "", baseSymbol = "USDT") {
         targetSymbol = targetSymbol.toUpperCase();
         baseSymbol = baseSymbol.toUpperCase();
         const pair = `${targetSymbol}/${baseSymbol}`;
@@ -379,7 +373,7 @@ class Bitmusa {
 
     }
 
-    wallet(){
+    wallet() {
         return new Promise((resolve, reject) => {
             request(this.buildRequestOptions("/users/asset/wallet", 'GET'), (error, response, body) => {
                 if (error)
@@ -388,23 +382,23 @@ class Bitmusa {
                     if (response.statusCode !== 200) {
                         reject("statusCode : " + response.statusCode);
                     }
-                        
+
                     let json = typeof body === 'object' ? body : JSON.parse(body);
                     if (json.code !== 0) {
-                            reject(json);
+                        reject(json);
                     } else {
-                            resolve(json);
-                    }   
+                        resolve(json);
+                    }
                 }
             });
         });
     }
 
-    latestTrade(targetSymbol = "", baseSymbol="USDT", size = 1){
+    latestTrade(targetSymbol = "", baseSymbol = "USDT", size = 1) {
         targetSymbol = targetSymbol.toUpperCase();
         baseSymbol = baseSymbol.toUpperCase();
         const pair = `${targetSymbol}/${baseSymbol}`;
-        
+
         return new Promise((resolve, reject) => {
             request(this.buildRequestOptions("/market/latest-trade", 'GET', { symbol: `${pair}`, size: size }), (error, response, body) => {
                 if (error)
@@ -416,16 +410,16 @@ class Bitmusa {
 
                     let json = typeof body === 'object' ? body : JSON.parse(body);
                     if (error) {
-                         reject(json);
+                        reject(json);
                     } else {
-                         resolve(json);
+                        resolve(json);
                     }
                 }
             });
         });
     }
 
-    orderbook(targetSymbol = "", baseSymbol="USDT"){
+    orderbook(targetSymbol = "", baseSymbol = "USDT") {
         targetSymbol = targetSymbol.toUpperCase();
         baseSymbol = baseSymbol.toUpperCase();
         const pair = `${targetSymbol}/${baseSymbol}`;
@@ -450,11 +444,11 @@ class Bitmusa {
         });
     }
 
-    fLatestTrade(targetSymbol = "", baseSymbol="TUSDT", size = 50){
+    fLatestTrade(targetSymbol = "", baseSymbol = "TUSDT", size = 50) {
         targetSymbol = targetSymbol.toUpperCase();
         baseSymbol = baseSymbol.toUpperCase();
         const pair = `${targetSymbol}${baseSymbol}`;
-        
+
         return new Promise((resolve, reject) => {
             request(this.buildRequestOptions("/future-market-trade/", 'GET', { ticker: `${pair}`, size: size }), (error, response, body) => {
                 if (error)
@@ -466,16 +460,16 @@ class Bitmusa {
 
                     let json = typeof body === 'object' ? body : JSON.parse(body);
                     if (error) {
-                         reject(json);
+                        reject(json);
                     } else {
-                         resolve(json);
+                        resolve(json);
                     }
                 }
             });
         });
     }
 
-    fOrderbook(targetSymbol = "", baseSymbol="TUSDT", size=50){
+    fOrderbook(targetSymbol = "", baseSymbol = "TUSDT", size = 50) {
         targetSymbol = targetSymbol.toUpperCase();
         baseSymbol = baseSymbol.toUpperCase();
         const pair = `${targetSymbol}${baseSymbol}`;
@@ -500,7 +494,7 @@ class Bitmusa {
         });
     }
 
-    fOpen(targetSymbol = "", baseSymbol="TUSDT", margin_mode=0, position="buy", order_type=1, leverage=10, order_price=1, order_qty=0){
+    fOpen(targetSymbol = "", baseSymbol = "TUSDT", margin_mode = 0, position = "buy", order_type = 1, leverage = 10, order_price = 1, order_qty = 0) {
         targetSymbol = targetSymbol.toUpperCase();
         baseSymbol = baseSymbol.toUpperCase();
         const pair = `${targetSymbol}${baseSymbol}`;
@@ -511,14 +505,14 @@ class Bitmusa {
         if (position == "SELL") position = 1; // short
 
         var options = {
-            direction : 0, // 0: Open, 1: Close
-            ticker : `${pair}`,
-            margin_mode : margin_mode,
-            position : position,
-            order_type : order_type,
-            leverage : leverage,
-            order_price : order_price,
-            order_qty : order_qty
+            direction: 0, // 0: Open, 1: Close
+            ticker: `${pair}`,
+            margin_mode: margin_mode,
+            position: position,
+            order_type: order_type,
+            leverage: leverage,
+            order_price: order_price,
+            order_qty: order_qty
         };
 
         return new Promise((resolve, reject) => {
@@ -542,7 +536,7 @@ class Bitmusa {
     }
 
 
-    fClose(targetSymbol = "", baseSymbol="TUSDT", margin_mode=0, position="buy", order_type=1, leverage=10, order_price=1, order_qty=0){
+    fClose(targetSymbol = "", baseSymbol = "TUSDT", margin_mode = 0, position = "buy", order_type = 1, leverage = 10, order_price = 1, order_qty = 0) {
         targetSymbol = targetSymbol.toUpperCase();
         baseSymbol = baseSymbol.toUpperCase();
         const pair = `${targetSymbol}${baseSymbol}`;
@@ -553,14 +547,14 @@ class Bitmusa {
         if (position == "SELL") position = 1; // short
 
         var options = {
-            direction : 1, // 0: Open, 1: Close
-            ticker : `${pair}`,
-            margin_mode : margin_mode,
-            position : position,
-            order_type : order_type,
-            leverage : leverage,
-            order_price : order_price,
-            order_qty : order_qty
+            direction: 1, // 0: Open, 1: Close
+            ticker: `${pair}`,
+            margin_mode: margin_mode,
+            position: position,
+            order_type: order_type,
+            leverage: leverage,
+            order_price: order_price,
+            order_qty: order_qty
         };
 
         return new Promise((resolve, reject) => {
@@ -583,12 +577,11 @@ class Bitmusa {
         });
     }
 
-    fCloseAll(targetSymbol = "", baseSymbol="TUSDT"){
+    fCloseAll(targetSymbol = "", baseSymbol = "TUSDT") {
         targetSymbol = targetSymbol.toUpperCase();
         baseSymbol = baseSymbol.toUpperCase();
         var pair = `${targetSymbol}${baseSymbol}`;
-        if (targetSymbol == "ALL")
-        {
+        if (targetSymbol == "ALL") {
             pair = "ALL";
         }
 
@@ -633,12 +626,11 @@ class Bitmusa {
         });
     }
 
-    fCancelAll(targetSymbol = "", baseSymbol="TUSDT"){
+    fCancelAll(targetSymbol = "", baseSymbol = "TUSDT") {
         targetSymbol = targetSymbol.toUpperCase();
         baseSymbol = baseSymbol.toUpperCase();
         var pair = `${targetSymbol}${baseSymbol}`;
-        if (targetSymbol == "ALL")
-        {
+        if (targetSymbol == "ALL") {
             pair = "ALL";
         }
 
@@ -661,7 +653,7 @@ class Bitmusa {
             });
         });
     }
-    
+
 }
 
 // export the class
