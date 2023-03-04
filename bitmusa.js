071cec0bbba916e62eb60c8d403d49634057516b
satoshi-bitninja <satoshi@bitninja.dev>
2023-03-05 04:46:44 +0900

    add fetchFutureTickers(), getFutureTicker()

--- bitmusa.js
+++ bitmusa.js
@@ -702,6 +702,61 @@ class Bitmusa {
         }
     }
 
+    async fetchFutureTickers() {
+        const funcName = '[fetchFutureTickers]:';
+
+
+        var options = {
+        };
+
+        try {
+            const response = await this.requestAPI('/future-board/', 'get', options);
+            if (response.status !== 200) throw new Error(`${funcName} ${response.status}`);
+            const json = response.data;
+            //console.log(json);
+            if ((json.code) && (json.code !== 0))
+            {
+                throw new Error(`${funcName} ${response.data.message}[code:${json.code}]`);
+            }
+
+            return json;
+        } catch (error) {
+            throw new Error(`${error.message}`);
+        }
+    }
+
+    async getFutureTicker(targetSymbol = null, baseSymbol = "TUSDT") {
+        const funcName = '[getFutureTicker]:';
+
+        if (!targetSymbol) throw new Error(`${funcName} targetSymbol is blank`);
+        targetSymbol = targetSymbol.toUpperCase();
+        baseSymbol = baseSymbol.toUpperCase();
+        const pair = `${targetSymbol}${baseSymbol}`;
+
+        var options = {
+            ticker: `${pair}`
+        };
+
+        try {
+            const response = await this.requestAPI('/future-board/', 'get', options);
+            if (response.status !== 200) throw new Error(`${funcName} ${response.status}`);
+            const json = response.data;
+            //console.log(json);
+            if ((json.code) && (json.code !== 0))
+            {
+                throw new Error(`${funcName} ${response.data.message}[code:${json.code}]`);
+            }
+
+            return json;
+        } catch (error) {
+            throw new Error(`${error.message}`);
+        }
+    }
+
+
+
+
+
     fCancel(order_id) {
         return new Promise((resolve, reject) => {
             request(this.buildRequestOptions(`/future-order/cancel/${order_id}`, 'PUT', {}), (error, response, body) => {
