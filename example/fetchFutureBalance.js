adf7b3dc4652abd8066f1d0dbd6dd0e43dc25e3e
satoshi-bitninja <satoshi@bitninja.dev>
2023-03-07 19:31:21 +0900

    Create fetchFutureBalance.js

---
+++ example/fetchFutureBalance.js
@@ -0,0 +1,31 @@
+const Bitmusa = require('../bitmusa');
+const bitmusa = new Bitmusa('a3f74de1-b23b-4758-9621-b4925a3876bd');
+
+(async function(){
+
+    bitmusa.setBaseURL("https://qa-api.teamneonex.com");
+
+    bitmusa.fetchFutureBalance().then(result => {
+        console.log(result);
+    }).catch(err => {
+        console.log(err);
+    });
+
+
+})();
+
+/*
+[
+  {
+    symbol: 'TUSDT',
+    total_wallet_balance: 100000,
+    total_margin_balance: 100000,
+    total_available_balance: 100000,
+    total_unpnl: 0,
+    position: []
+  }
+]
+
+{ code: 4000, message: 'need sign in' }
+
+*/
